076694415354a2108cc3c1779bfd1a3dfc1c09fd
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-18 16:55:46 -0500

    added some stylings and animation

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,21 +4,29 @@ import styled from 'styled-components';
 
 const StyledContainer = styled.div`
     display: flex;
-    justify-content: space-between;   
+    justify-content: space-around;   
     flex-direction: row;
     background-color: #1d2d44;
+    width: 90%;
+    align-items:right;
+    border-style: dashed;
+    animation-duration: 3s;
+    animation-name: slidein;
 `
 const StyledCharacterName = styled.h1`
     font-size: 3rem;
-    color: #f0ebd8;
+    color: #90caf9;
 `
-
+const StyledBirthYear = styled.h2`
+    color:white;
+    font-size:2rem;
+`
 
 const Character = (props)=>{
     return(
         <StyledContainer>
             <StyledCharacterName >{props.character.name}</StyledCharacterName>
-
+            <StyledBirthYear>{props.character['birth_year']}</StyledBirthYear>
         </StyledContainer>
     )
 }
