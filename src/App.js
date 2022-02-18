3481af30eb4f23e665141f8462e04cc3ec5eccb6
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-18 16:19:21 -0500

    Finished MVP, added styled components to character.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters,setCharacters] = useState(null);
+  const [characters,setCharacters] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -14,15 +14,20 @@ const App = () => {
     axios.get(`https://swapi.dev/api/people`)
     .then(response =>{
       console.log(response.data);
+      //Data is held within body array of Response object
       setCharacters(response.data)
+      console.log(characters);
     })
     .catch(error => console.log(error));
   },[])
   return (
     <div className="App">
-      {characters && <Character character = {characters}/>}
+      {
+      characters.map((character)=>{
+        return <Character character = {character}/>
+      })
+    }
     </div>
   );
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,25 @@
 // Write your Character component here
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
-const Character = (props)=>{
+const StyledContainer = styled.div`
+    display: flex;
+    justify-content: space-between;   
+    flex-direction: row;
+    background-color: #1d2d44;
+`
+const StyledCharacterName = styled.h1`
+    font-size: 3rem;
+    color: #f0ebd8;
+`
 
+
+const Character = (props)=>{
+    return(
+        <StyledContainer>
+            <StyledCharacterName >{props.character.name}</StyledCharacterName>
+
+        </StyledContainer>
+    )
 }
 export default Character;
